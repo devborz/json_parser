6a0454d5819d0eb6e694482df3ddcc19cfa1c863
devborz <u.turkaev@yandex.ru>
2019-10-01 20:34:53 +0000

    sixth commit

--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devborz/devborz/workspace/sources/lab-01-parser
-# Build directory: /home/devborz/devborz/workspace/sources/lab-01-parser/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/devborz/devborz/workspace/sources/lab-01-parser/_build/tests")
